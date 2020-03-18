2476005d0c610bd64743e7b3cd8d7cd9699545b1
unknown <omar.navarro@g.austincc.edu>
2020-03-17 20:07:57 -0500

    TODO pop message from queue

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueApp
+{
+  class Program
+  {
+    // use key vault instead
+    private const string ConnectionString = "";
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Holi Mundo!");
+    }
+    static async Task SendArticleAsync(string newsMessage)
+    {
+      CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+      CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+      CloudQueue queue = queueClient.GetQueueReference("newsqueue");
+      bool createdQueue = await queue.CreateIfNotExistsAsync();
+      if (createdQueue)
+      {
+        Console.WriteLine("The queue of news articles was created.");
+      }
+
+      CloudQueueMessage articleMessage = new CloudQueueMessage(newsMessage);
+      await queue.AddMessageAsync(articleMessage);
+    }
+  }
+}
